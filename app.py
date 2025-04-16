9764fc1bbb3e64837cd8f3c21aaec6205ecdba80
sharanya-sharma <iamsharanyasharma@gmail.com>
2025-04-16 23:11:36 +0530

    Set app to run on 0.0.0.0 and dynamic port for Render

--- app.py
+++ app.py
@@ -82,4 +82,6 @@ def merge_videos():
         return jsonify(success=False, error=str(e))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
